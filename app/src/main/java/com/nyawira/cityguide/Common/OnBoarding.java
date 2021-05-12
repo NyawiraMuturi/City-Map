3e2633dcfaa86ea525bfa38491c2013e129f57a4
Nyawira Muturi <albinamuturi@gmail.com>
2021-05-12 18:00:08 +0300

    add slide show design

---
+++ app/src/main/java/com/nyawira/cityguide/Common/OnBoarding.java
@@ -0,0 +1,16 @@
+package com.nyawira.cityguide.Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nyawira.cityguide.R;
+
+public class OnBoarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
