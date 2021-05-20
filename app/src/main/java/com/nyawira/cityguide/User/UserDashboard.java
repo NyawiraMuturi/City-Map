c20bafe4010df1767e461067822f810185fb16c2
Nyawira Muturi <albinamuturi@gmail.com>
2021-05-20 15:54:38 +0300

    prep recycler view functions and orientation

--- app/src/main/java/com/nyawira/cityguide/User/UserDashboard.java
+++ app/src/main/java/com/nyawira/cityguide/User/UserDashboard.java
@@ -1,6 +1,8 @@
 package com.nyawira.cityguide.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Window;
@@ -10,11 +12,27 @@ import com.nyawira.cityguide.R;
 
 public class UserDashboard extends AppCompatActivity {
 
+    //variables
+    RecyclerView featured, popular, category;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
+
+        //hooks
+        featured = findViewById(R.id.featured);
+        popular = findViewById(R.id.popular);
+        category = findViewById(R.id.categories);
+
+        featuredRecycler();
+    }
+
+    private void featuredRecycler() {
+
+        featured.setHasFixedSize(true);
+        featured.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
     }
 }
