aa5c5946294161ca3998eeb2f32426e692b143ae
Nyawira Muturi <albinamuturi@gmail.com>
2021-05-24 15:42:31 +0300

    create category helper class

--- app/src/main/java/com/nyawira/cityguide/HelperClasses/HomeAdapter/CategoriesHelperClass.java
+++ app/src/main/java/com/nyawira/cityguide/HelperClasses/HomeAdapter/CategoriesHelperClass.java
@@ -1,4 +1,22 @@
 package com.nyawira.cityguide.HelperClasses.HomeAdapter;
 
+import android.widget.TextView;
+
 public class CategoriesHelperClass {
+
+    String categoryName;
+    int image;
+
+    public CategoriesHelperClass(String categoryName, int image) {
+        this.categoryName = categoryName;
+        this.image = image;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public int getImage() {
+        return image;
+    }
 }
