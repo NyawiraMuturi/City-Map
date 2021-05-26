9bd9f2fbdcdea3854609d22380ec24927c927472
Nyawira Muturi <albinamuturi@gmail.com>
2021-05-26 16:53:39 +0300

    add functionality to buttons

--- app/src/main/java/com/nyawira/cityguide/User/AllCategories.java
+++ app/src/main/java/com/nyawira/cityguide/User/AllCategories.java
@@ -3,14 +3,29 @@ package com.nyawira.cityguide.User;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.nyawira.cityguide.R;
 
 public class AllCategories extends AppCompatActivity {
 
+    //variables
+    ImageView backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_categories);
+
+        //hooks
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AllCategories.super.onBackPressed();
+            }
+        });
     }
 }
