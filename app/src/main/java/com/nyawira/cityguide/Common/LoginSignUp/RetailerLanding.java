b42bf210e9543ee5ada565f93b2e66c504ecad1c
Nyawira Muturi <albinamuturi@gmail.com>
2021-05-31 14:37:38 +0300

    add retailer landing page

---
+++ app/src/main/java/com/nyawira/cityguide/Common/LoginSignUp/RetailerLanding.java
@@ -0,0 +1,16 @@
+package com.nyawira.cityguide.Common.LoginSignUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nyawira.cityguide.R;
+
+public class RetailerLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retailer_landing);
+    }
+}
