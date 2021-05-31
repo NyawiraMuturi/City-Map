1915914edd15c8e71d0a78ccdf1063e72be62506
Nyawira Muturi <albinamuturi@gmail.com>
2021-05-31 16:50:37 +0300

    finish material design login

--- app/src/main/java/com/nyawira/cityguide/Common/LoginSignUp/SignUpActivity.java
+++ app/src/main/java/com/nyawira/cityguide/Common/LoginSignUp/SignUpActivity.java
@@ -8,13 +8,13 @@ import android.view.WindowManager;
 
 import com.nyawira.cityguide.R;
 
-public class RetailerLanding extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_retailer_landing);
+        setContentView(R.layout.activity_sign_up);
     }
 }
