5110f50d6b222e60d51e68037500c7d2d725e928
Nyawira Muturi <albinamuturi@gmail.com>
2021-06-02 23:49:16 +0300

    link design

--- app/src/main/java/com/nyawira/cityguide/Common/LoginSignUp/SignUpActivity.java
+++ app/src/main/java/com/nyawira/cityguide/Common/LoginSignUp/SignUpActivity.java
@@ -3,12 +3,19 @@ package com.nyawira.cityguide.Common.LoginSignUp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.nyawira.cityguide.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //variables
+    ImageView backButton;
+    Button nextBtn, loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,21 @@ public class SignUpActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
+
+        //hooks
+        backButton = findViewById(R.id.backBtn);
+        nextBtn = findViewById(R.id.nextBtn);
+        loginBtn = findViewById(R.id.login);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignUpActivity.super.onBackPressed();
+            }
+        });
+    }
+
+    public void callNextScreen (View view){
+
     }
 }
