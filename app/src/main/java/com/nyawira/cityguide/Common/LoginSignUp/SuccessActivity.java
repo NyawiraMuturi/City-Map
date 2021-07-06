73ff7c32bf6c7b60b18e2e8636b636d211cfe936
Nyawira Muturi <albinamuturi@gmail.com>
2021-07-06 16:14:46 +0300

    finish success screen UI

---
+++ app/src/main/java/com/nyawira/cityguide/Common/LoginSignUp/SuccessActivity.java
@@ -0,0 +1,16 @@
+package com.nyawira.cityguide.Common.LoginSignUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nyawira.cityguide.R;
+
+public class SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
